2cfa7b310af4bd4697aacd7d4e16ac696ccb05a0
Student <Student@Students-Mac-mini.local>
2016-07-28 16:14:30 -0600

    working

--- document.js
+++ document.js
@@ -3,6 +3,7 @@
   console.log('document.js:loaded')
 //define page object
   var MyClass = function(){}
+  var myApp = new MyClass()
 //create prototype init and bind events to the page object
   MyClass.prototype.init = function()
     {
@@ -17,37 +18,42 @@
 //create prototype getSymbol. getsymbol will create a variable symbol
 // and set the value to the attr "data-ticker" of the selectedevent
   MyClass.prototype.getSymbol = function(event){
-      var symbol = $(event.target).attr('data-ticker')
+    var symbol = $(event.target).attr('data-ticker')
 
-      console.log(symbol)
+    console.log(symbol)
+
+    myApp.getQuote(symbol)
   }
-var myApp = new MyClass()
 
+//create prototype getQuote.this is going to use the symbol retrieved from the getSymbol
+  MyClass.prototype.getQuote = function(symbol) {
+    //do ajax and pass 'symbol' to the query string
+    //when it completes  attach a promise,done or fail
+    $.ajax({
+    url: "http://dev.markitondemand.com/Api/v2/Quote/jsonp",
+    data:{
+      symbol:symbol,
+    },
+    type:'get',
+    dataType:'jsonp '
 
+  }).done(function(response) {
+      console.log(response);
+      $('#headName').html(response.Name);
+      $('#headSymbol').html(response.Symbol);
+      $('#CompanyNameCell').html(response.Name);
+      $('#CompanySymbolCell').html(response.Symbol);
+      $('#LastPriceCell').html(response.LastPrice);
+      $('#ChangeCell').html(response.Change);
+      $('#ChangePercentCell').html(response.ChangePercent);
+      $('#ChangeYTDCell').html(response.ChangePercentYTD);
+      $('#OpenCell').html(response.Open);
+      $('#HighCell').html(response.High);
+      $('#LowCell').html(response.Low);
+  })
+    .fail(function() { alert("error"); });
+  }
 
-
-
-
-// //create prototype getQuote.this is going to use the symbol retrieved from the getSymbol
-//   MyClass.prototype.getQuote = function(symbol) {
-//     //do ajax and pass 'symbol' to the query string
-//     //when it completes  attach a promise,done or fail
-//
-//
-//     $.ajax({
-//     url: "http://dev.markitondemand.com/Api/v2/Quote",
-//     data:{
-//       symbol:symbol,
-//     },
-//     type:'get',
-//     dataType:'jsonp'
-//
-//   }).done(function(response) {
-//     console.log(response);
-//   })
-//       // .fail(function() { alert("error"); });
-//
-//   }
 
 
 
@@ -56,15 +62,8 @@ var myApp = new MyClass()
 $('document').ready(function(){
 // on the click event ,call a function that will
 // get the info of data ticker from the selected div
-
-// $('div.grid-element').on('click', $(myApp.getSymbol);
-
-$("div.grid-element").on("click", function(event){
-  console.log('click success');
-  myApp.getSymbol(event)
-});
-
-
-
-
+  $("div.grid-element").on("click", function(event){
+    console.log('click success');
+    myApp.getSymbol(event)
+  });
 })
